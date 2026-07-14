6b0ab609e768472d01e3d57299d4d0b0ee02f37a
duranabner <abneer3112@gmail.com>
2026-07-13 23:21:28 -0300

    Add Todo types definitions

---
+++ app/types/todo.ts
@@ -0,0 +1,18 @@
+export interface Todo {
+  id: string;
+  title: string;
+  description?: string;
+  completed: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface TodoContextType {
+  todos: Todo[];
+  addTodo: (title: string, description?: string) => void;
+  updateTodo: (id: string, updates: Partial<Todo>) => void;
+  deleteTodo: (id: string) => void;
+  toggleTodo: (id: string) => void;
+  clearCompleted: () => void;
+  loading: boolean;
+}
